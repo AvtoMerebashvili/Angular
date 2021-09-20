9a87dfcef2a7b0ab1bac34ac442d24cdd878661f
Avtandil-1 <avtandil.merebashvili.2@gmail.com>
2021-09-20 15:46:45 +0400

    HOMEWORK: 2

--- 3-Forms/homework1/src/app/homework2/currency/currency.component.ts
+++ 3-Forms/homework1/src/app/homework2/currency/currency.component.ts
@@ -16,10 +16,12 @@ export class CurrencyComponent implements OnInit {
   inputValues:number[] = [];
 
   equivalent:number | string = "";
+  added:boolean = false;
 
   $firstOpt = new Subject<number>();
   $secondOpt = new Subject<number>();
-
+  secondValue:number|string = "";
+  
   constructor(
     private httpData: HttpServiceService,
   ) { }
@@ -37,16 +39,31 @@ export class CurrencyComponent implements OnInit {
 
   onAdd(){
     this.inputFields.push(this.inputFields.length);
+    this.secondValue = "";
+    this.added = true
   }
 
   firstInput(value:any, num:number){
     this.inputParams[num] = {...this.inputParams[num], amount: value.target.value}
-    this.checkAllFiedls()  
+    this.checkAllFiedls() 
   }
- 
-  firstOption(value:any, num:number){
+
+  secondInput(value:any){
+    if(!this.added){
+      this.equivalent=0;
+      this.inputFields = [0];
+      this.inputParams = [];
+      this.inputValues = [];
+      this.countMoney(value)
+    }
+    
+
+  }
+
+  firstOption(value:any, num:number = 0){
+    this.$firstOpt = value.target.value
     this.inputParams[num] = {...this.inputParams[num], course: value.target.value}  
-    this.checkAllFiedls()  
+    this.checkAllFiedls() 
   }
 
   secondtOption(value:any){
@@ -54,16 +71,27 @@ export class CurrencyComponent implements OnInit {
     this.checkAllFiedls()
   }
 
-  private countMoney(){
-    if(typeof this.$secondOpt == 'string'){
-        this.inputParams.map((field,i)=>{
-          if(Object.keys(field).length == 2){
-            let course = Number(this.$secondOpt)/Number(field.course);
-            let money =  Number(field.amount)*course;
-            this.inputValues[i]=money;
-          }
-        })
-        this.inputValues.length > 0 ? this.equivalent = this.inputValues.reduce((sum, i) => sum+i) : this.inputValues
+  private countMoney(value:any = 0){
+    if(value == 0){
+      if(typeof this.$secondOpt == 'string'){
+          this.inputParams.map((field,i)=>{
+            if(Object.keys(field).length == 2){
+              let course = Number(this.$secondOpt)/Number(field.course);
+              let money =  Number(field.amount)*course;
+              this.inputValues[i]=money;
+            }
+          })
+          this.inputValues.length > 0 ? this.equivalent = this.inputValues.reduce((sum, i) => sum+i) : this.inputValues
+      }
+    }else{
+      if(typeof this.$firstOpt == 'string'){
+        let course = (Number(this.$firstOpt) /(Number(this.$secondOpt) ));
+        let money = value.target.value*course
+        this.secondValue = money
+        this.inputParams[0] = {course:  this.$firstOpt, amount: 0}
+
+      }
+      
     }
   }
 
