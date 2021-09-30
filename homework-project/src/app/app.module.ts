cbe6e2e3964bdb1096ffaabbcb266e6f6b9ff668
Avtandil-1 <avtandil.merebashvili.2@gmail.com>
2021-10-01 01:19:00 +0400

    update folder structure

--- homework-project/src/app/app.module.ts
+++ homework-project/src/app/app.module.ts
@@ -4,20 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RegistrationComponent } from './homewrok1/registration/registration.component';
-import { ListComponent } from './homewrok1/list/list.component';
-import { UpdateComponent } from './homewrok1/update/update.component';
+import { RegistrationComponent } from './homewrok1-4/registration/registration.component';
+import { ListComponent } from './homewrok1-4/list/list.component';
+import { UpdateComponent } from './homewrok1-4/update/update.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
-import { Homewrok1Component } from './homewrok1/homewrok1.component';
+import { Homewrok1Component } from './homewrok1-4/homewrok1.component';
 import { RouterModule } from '@angular/router';
 import { Homework2Component } from './homework2/homework2.component';
 import { CurrencyComponent } from './homework2/currency/currency.component';
 import { HttpClientModule } from '@angular/common/http';
 import { PeopleModule } from './homework3/people/people.module';
-import { UserComponent } from './homewrok1/list/user/user.component';
-import { LoginComponent } from './homewrok1/login/login.component';
-import { LoginGuard } from './homewrok1/guards/login.guard';
-import { SignGuard } from './homewrok1/guards/sign.guard';
+import { UserComponent } from './homewrok1-4/list/user/user.component';
+import { LoginComponent } from './homewrok1-4/login/login.component';
+import { LoginGuard } from './homewrok1-4/guards/login.guard';
+import { SignGuard } from './homewrok1-4/guards/sign.guard';
 
 @NgModule({
   declarations: [
